bf7a6c1c5659ffa19a40caacc6a4b55c74195ed4
JiangWuxian <68415175+JiangWuxian@users.noreply.github.com>
2020-10-23 19:08:05 -0500

    HoHo

--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
-
+const sumAll = function (x, y) {
+    let sum = 0;
+    if (typeof (x) != typeof (0) || typeof (y) != typeof (0)) {
+        return 'ERROR';
+    } else if (y < 0 || x < 0) {
+        return 'ERROR';
+    } else if (x < y) {
+        for (x; x <= y; x++) {
+            sum += x;
+        }
+        return sum;
+    } else if (x > y) {
+        for (y; y <= x; y++) {
+            sum += y;
+        }
+        return sum;
+    } else {
+        return x;
+    }
 }
 
 module.exports = sumAll
